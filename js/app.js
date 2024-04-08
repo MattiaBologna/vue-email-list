ff0b796f626b791aa8f7bda114dc82dd31c619e8
MattiaBologna <mattiabologna01@gmail.com>
2024-04-08 16:00:08 +0200

    Add: mails print

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log(axios)
+console.log('API')
+
+const mailList = document.getElementById('mail--list')
+
+for (i = 0; i < 10; i++) {
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(function (res) {
+    console.log(res.data.response)
+
+    mailList.innerHTML += `<li>${res.data.response}</li>`
+    
+    })
+}
