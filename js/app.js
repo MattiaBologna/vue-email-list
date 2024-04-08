413ca4ad8ff160b6e06eab242c031608be59dd8e
MattiaBologna <mattiabologna01@gmail.com>
2024-04-08 17:59:11 +0200

    Fix: vue start

--- js/app.js
+++ js/app.js
@@ -1,13 +1,36 @@
 console.log('API')
 
-const mailList = document.getElementById('mail--list')
+const { createApp } = Vue
 
-for (i = 0; i < 10; i++) {
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(function (res) {
-    console.log(res.data.response)
+  createApp({
+    data() {
+      return {
+        mailList: [],
+      }
+    },
+    methods: {
+        fetchMail() {
+            for (i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(function (res) {
+                const mail = res.data.response
+                console.log(`mail: `, mail)
+                this.mailList.push(mail)
+            })
+        }
+        console.log('array mail: ', this.mailList)
+        }
+    }
+  }).mount('#app')
 
-    mailList.innerHTML += `<li>${res.data.response}</li>`
+// const mailList = document.getElementById('mail--list')
+
+// for (i = 0; i < 10; i++) {
+//     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+//     .then(function (res) {
+//     console.log(res.data.response)
+
+//     mailList.innerHTML += `<li>${res.data.response}</li>`
     
-    })
-}
+//     })
+// }
